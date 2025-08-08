18cab7a34c7ef4e4eda602c20e8e52c18bf3942c
apple <apple@appleui-MacBookAir.local>
2025-08-08 11:38:14 +0900

    추가 작업을 위한 미완성 파일들 업로드

---
+++ 0807_JS/index.js
@@ -0,0 +1,13 @@
+// 콘솔 활용하기
+console.log("example");
+console.log(1,2,3);
+
+// let a;
+a = 10;
+console.log(a);
+
+let name_a = "홍길동";
+let text_a = "안녕하세요 저는 " + name_a + " 입니다.";
+console.log(text_a)
+
+let text_b = `가나다라마 ${name_a} 입니다.`;
